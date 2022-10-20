81cd16da0e56a3f47647904f3f5310b99eb645e8
touhidcodes <touhidcodes@gmail.com>
2022-10-20 13:25:18 +0600

    phone count & price

--- Js/common.js
+++ Js/common.js
@@ -1,12 +1,10 @@
-document
-  .getElementById("btn-phone-plus")
-  .addEventListener("click", function () {
-    getTextElementValueById("btn-phone-plus");
-    updatePhoneNumber(true);
-  });
-
 function getTextElementValueById(elementId) {
-  const currentPhoneValue = document.getElementById(elementId).value;
+  const currentPhoneValue = document.getElementById(elementId).innerText;
   const currentPhoneString = parseInt(currentPhoneValue);
   return currentPhoneString;
 }
+
+document.getElementById("remove").addEventListener("click", function () {
+  const constValuePhone = document.getElementById("totalPhonePrice");
+  constValuePhone = 1219;
+});
--- Js/phone.js
+++ Js/phone.js
@@ -5,7 +5,7 @@ function updatePhoneNumber(isIncreased) {
 
   let newPhoneNumber;
 
-  if (isIncreased) {
+  if (isIncreased == true) {
     newPhoneNumber = previousValueString + 1;
   } else {
     newPhoneNumber = previousValueString - 1;
@@ -13,3 +13,24 @@ function updatePhoneNumber(isIncreased) {
   document.getElementById("phone-number-field").value = newPhoneNumber;
   return newPhoneNumber;
 }
+
+function updatePhoneTotalPrice(newPhoneNumber) {
+  const phoneTotalPrice = newPhoneNumber * 1219;
+  document.getElementById("totalPhonePrice").innerText = phoneTotalPrice;
+}
+// Phone Count Increase
+document
+  .getElementById("btn-phone-plus")
+  .addEventListener("click", function () {
+    const newPhoneNumber = updatePhoneNumber(true);
+
+    updatePhoneTotalPrice(newPhoneNumber);
+  });
+// Phone Count decrease
+document
+  .getElementById("btn-phone-minus")
+  .addEventListener("click", function () {
+    const newPhoneNumber = updatePhoneNumber(false);
+
+    updatePhoneTotalPrice(newPhoneNumber);
+  });
